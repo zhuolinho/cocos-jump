b59a769c54d831f3df6205d957e936513c50c688
zhuolinho <zhuolinho@hotmail.com>
2019-06-03 13:48:57 +0800

    add const

---
+++ assets/Const/Const.js
@@ -0,0 +1,30 @@
+module.exports = {
+    // me
+    userID : 0,
+    userName: "",
+    avatarUrl: "",
+    // game
+    SCENT_WIDTH: 0,
+    SCENT_HEIGHT: 0,
+
+    gameVersion: 1,
+    channel: "Matchvs",
+    platform: "alpha",//alpha选项必须开启gameserver本地调试，默认release
+    // TEST:
+    // platform: "alpha",
+    deviceId: "123456789",
+    gatewayId: 0,
+    token: "",
+    appKey: '8c9225f7300e4e3da7f429fc16faabe2#M',
+    // test
+    gameID: 216156,
+    secretKey: '170313f9147a4fdf9b9b56a082630369',
+    // client room event
+    GAME_START_EVENT: 'game_start_event',
+
+    FPS:20,
+    SPEED_UP:2,
+    SPEED:3,
+    SPEED_DISSIPATION_SCORE : 3,
+
+};
